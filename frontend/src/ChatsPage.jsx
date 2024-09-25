b87894b4989e79b2c8920760a3139f5b75963f81
Katende Chris Marvin <85847700+256-katendechris-256@users.noreply.github.com>
2024-09-25 20:24:21 +0300

    Update ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,10 +1,10 @@
-import { PrettyChatWindow } from 'react-chat-engine-pretty;
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = ({ user }) => {
   return (
     <div style={{ height: '100vh' }}>
       <PrettyChatWindow
-        projectId='1e4dc223-ee8b-4795-a656-55152f63b152
+        projectId='1e4dc223-ee8b-4795-a656-55152f63b152'
         username={user.username}  // Access 'user' directly since it's destructured
         secret={user.secret}      // Similarly for the secret
         style={{ height: '100%' }}
